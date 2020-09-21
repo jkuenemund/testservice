8fffccb80a7f95fefc1ab0b5629d2be103c662bd
Jens Künemund <jens.kuenemund@senacor.com>
2020-09-21 14:52:30 +0200

    Unit Tests added

---
+++ src/test/java/com/innolab/testservice/rest/CalcControllerTest.java
@@ -0,0 +1,37 @@
+package com.innolab.testservice.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CalcControllerTest {
+
+    CalcController sut;
+
+    @BeforeEach
+    void setup() {
+        sut = new CalcController();
+    }
+
+    @Test
+    void test01() {
+        String methods = sut.getMethods();
+
+        assertThat(methods).isEqualTo("addition; subtraction; kill");
+    }
+
+    @Test
+    void test02() {
+        int addition = sut.getAddition(1, 3);
+
+        assertThat(addition).isEqualTo(4);
+    }
+
+    @Test
+    void test03() {
+        int addition = sut.getSubtraction(15, 3);
+
+        assertThat(addition).isEqualTo(12);
+    }
+}
